edcd9e30981611e0966b4206d268123a98be34cb
Parth Sharma <sharmaparth1208@gmail.com>
2024-02-08 15:43:33 +0530

    First commit

---
+++ src/test/java/com/example/spe_task/SpeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spe_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
